ea9808bfb5bd0633048faf4ff93527d0bb81ae1d
Larbol624 <lars.smallenbroek@ziggo.nl>
2026-01-26 19:40:59 +0100

    bug: shell needs to LF but turns into CRLF because of git feat:worked on streaming

--- configs/kafka/create_topics.sh
+++ configs/kafka/create_topics.sh
@@ -1,36 +1,9 @@
-echo"[INFO] CREATE_TOPICS is doing something"
+/usr/bin/kafka-topics --create --topic sensor_raw --bootstrap-server kafka:9092 --partitions 6 --replication-factor 1 --if-not-exists
 
-/usr/bin/kafka-topics --create \
-  --topic sensor_raw \
-  --bootstrap-server kafka:9092 \
-  --partitions 6 \
-  --replication-factor 1 \
-  --if-not-exists
+/usr/bin/kafka-topics --create --topic sensor_cleaned --bootstrap-server kafka:9092 --partitions 6 --replication-factor 1 --if-not-exists
 
-/usr/bin/kafka-topics --create \
-  --topic sensor_cleaned \
-  --bootstrap-server kafka:9092 \
-  --partitions 6 \
-  --replication-factor 1 \
-  --if-not-exists
+/usr/bin/kafka-topics --create --topic sensor_metrics --bootstrap-server kafka:9092 --partitions 6 --replication-factor 1 --if-not-exists
 
-/usr/bin/kafka-topics --create \
-  --topic sensor_metrics \
-  --bootstrap-server kafka:9092 \
-  --partitions 6 \
-  --replication-factor 1 \
-  --if-not-exists
+/usr/bin/kafka-topics --create --topic sensor_alerts --bootstrap-server kafka:9092 --partitions 3 --replication-factor 1 --if-not-exists
 
-/usr/bin/kafka-topics --create \
-  --topic sensor_alerts \
-  --bootstrap-server kafka:9092 \
-  --partitions 3 \
-  --replication-factor 1 \
-  --if-not-exists
-
-/usr/bin/kafka-topics --create \
-  --topic sensor_dlq \
-  --bootstrap-server kafka:9092 \
-  --partitions 3 \
-  --replication-factor 1 \
-  --if-not-exists
+/usr/bin/kafka-topics --create --topic sensor_dlq --bootstrap-server kafka:9092 --partitions 3 --replication-factor 1 --if-not-exists
