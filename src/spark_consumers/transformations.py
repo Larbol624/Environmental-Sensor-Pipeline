0cece7e20e96b9a1c695ec259112d1a6c19442ea
Larbol624 <lars.smallenbroek@ziggo.nl>
2026-02-08 21:57:27 +0100

    test: Finished tests and did some small fixes

--- src/spark_consumers/transformations.py
+++ src/spark_consumers/transformations.py
@@ -1,27 +1,24 @@
-from pyspark.sql.functions import col, from_json, try_to_timestamp, to_json, when, coalesce, expr
-from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType, TimestampType
+from pyspark.sql.functions import col, when, coalesce, expr, lit
 
-def clean_sensor_data(df,batch_id,output):
+def clean_sensor_data(df):
     
-    sensor_schema = StructType([
-        StructField("sensor_id", IntegerType()),
-        StructField("timestamp", StringType()),
-        StructField("temperature", DoubleType()),
-        StructField("humidity", DoubleType()),
-        StructField("co2", DoubleType())
-    ])
-        
+    EXPECTED_COLUMNS = ["sensor_id", "timestamp", "temperature", "humidity", "co2"]
+
+    for column in EXPECTED_COLUMNS:
+        if column not in df.columns:
+            df=df.withColumn(column,lit(None))
+    
     schemad_df=(
             df
-            .withColumn("sensor_id",col("sensor_id").cast(IntegerType()))
+            .withColumn("sensor_id", expr("try_cast(sensor_id as int)"))
             .withColumn("timestamp",
                 coalesce(
                 expr("try_to_timestamp(timestamp, 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS')"),
                 expr("try_to_timestamp(timestamp, 'yyyy-MM-dd''T''HH:mm:ss')")
             ))
-            .withColumn("temperature",col("temperature").cast(DoubleType()))
-            .withColumn("humidity",col("humidity").cast(DoubleType()))
-            .withColumn("co2",col("co2").cast(DoubleType()))
+            .withColumn("temperature", expr("try_cast(temperature as double)"))
+            .withColumn("humidity",expr("try_cast(humidity as double)"))
+            .withColumn("co2",expr("try_cast(co2 as int)"))
         )
 
     df_with_reason=schemad_df.withColumn(
@@ -36,11 +33,10 @@ def clean_sensor_data(df,batch_id,output):
     valid_df = df_with_reason.filter(col("dlq_reason").isNull())
     dlq_df   = df_with_reason.filter(col("dlq_reason").isNotNull())
 
-
+    return dlq_df, valid_df
     
 
-    output["valid"].append(valid_df.count())
-    output["invalid"].append(dlq_df.count())
+    
 
 
         
---
+++ tests/test_spark/test_schema-validation.py
@@ -0,0 +1,163 @@
+from src.spark_consumers.transformations import clean_sensor_data 
+from pyspark.sql.types import (
+    StructType, StructField,
+    IntegerType, DoubleType, TimestampType, StringType
+)
+
+def test_schema(spark):
+
+    valid_schema={
+        "sensor_id": 1,
+        "timestamp": '2026-01-30T21:00:00',
+        "temperature": 21.0,
+        "humidity": 50.0,
+        "co2":500
+    }
+
+    valid_df=spark.createDataFrame([valid_schema])
+    dlq,valid=clean_sensor_data(valid_df)
+
+    
+    assert valid.columns == ["co2","humidity","sensor_id","temperature","timestamp","dlq_reason"]
+
+    invalid_schema={
+        "sensor_d": 1,
+        "timestamp": '2026-01-30T21:00:00',
+        "temperature": 21.0,
+        "humidity": 50.0,
+        "co2":500
+    }
+
+    invalid_df=spark.createDataFrame([invalid_schema])
+    dlq,valid=clean_sensor_data(invalid_df)
+
+    assert not dlq.rdd.isEmpty()
+    assert valid.rdd.isEmpty()
+
+
+    invalid_df2=spark.createDataFrame(
+        [(
+            "a",
+            "21",
+            "123124124",
+            ""
+        )],
+        ["aslfjasf","lasdjsald","sladhaksjfh","askdhasf"]
+    )
+
+    dlq,valid=clean_sensor_data(invalid_df2)
+
+    assert not dlq.rdd.isEmpty()
+    assert valid.rdd.isEmpty()
+
+
+    missing_values_df=spark.createDataFrame([
+        {
+            "sensor_id":None,
+            "timestamp": '2026-01-30T21:00:00',
+            "temperature": 21.0,
+            "humidity": 50.0,
+            "co2":500
+        },
+        {
+            "sensor_id":1,
+            "timestamp": None,
+            "temperature": 21.0,
+            "humidity": 50.0,
+            "co2":500
+        },
+        {
+            "sensor_id":None,
+            "timestamp": None,
+            "temperature": 21.0,
+            "humidity": 50.0,
+            "co2":500
+        },
+        {
+            "sensor_id":1,
+            "timestamp": '2026-01-30T21:00:00',
+            "temperature": None,
+            "humidity": None,
+            "co2":None
+        }
+    ])
+
+    dlq,valid=clean_sensor_data(missing_values_df)
+
+    assert dlq.count() == 3
+    assert valid.count() == 1
+
+
+    wrong_types_df=spark.createDataFrame([
+        {
+            "sensor_id":"a",
+            "timestamp": '2026-01-30T21:00:00',
+            "temperature": 21.0,
+            "humidity": 40.0,
+            "co2":1233
+        },
+        {
+            "sensor_id":1,
+            "timestamp": '2026-01-30',
+            "temperature": 21.0,
+            "humidity": 40.0,
+            "co2":1233
+        },
+        {
+            "sensor_id":1,
+            "timestamp": 2,
+            "temperature": 21.0,
+            "humidity": 40.0,
+            "co2":1233
+        },
+        {
+            "sensor_id":1,
+            "timestamp": '2026-01-30T21:00:00.000000',
+            "temperature": 21.0,
+            "humidity": 40.0,
+            "co2":1233
+        },
+        {
+            "sensor_id":1,
+            "timestamp": '2026-01-30T21:00:00.00',
+            "temperature": "21312",
+            "humidity": 40.0,
+            "co2":1233
+        }
+    ])
+
+    dlq,valid=clean_sensor_data(wrong_types_df)
+
+    assert dlq.count() == 4
+
+    assert valid.count() == 1
+
+
+    ranges_df=spark.createDataFrame([
+        {
+            "sensor_id":1,
+            "timestamp": '2026-01-30T21:00:00.00',
+            "temperature": 25,
+            "humidity": 40.0,
+            "co2":1233
+        },
+        {
+            "sensor_id":1,
+            "timestamp": '2026-01-30T21:00:00.00',
+            "temperature": 125,
+            "humidity": 40.0,
+            "co2":1233
+        },
+        {
+            "sensor_id":1,
+            "timestamp": '2026-01-30T21:00:00.00',
+            "temperature": 25,
+            "humidity": 500000.0,
+            "co2":-10
+        }
+    ])
+
+    dlq,valid=clean_sensor_data(ranges_df)
+
+    assert dlq.count()      == 2
+    assert valid.count()    == 1
